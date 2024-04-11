7987ec9a475c607b256dd720611b33941db37715
KevinDev115 <kevin.galindo@draketech.ca>
2024-04-10 20:43:07 -0600

    feat(journal): App layout and journal page

--- src/app/pages/JournalPage.jsx
+++ src/app/pages/JournalPage.jsx
@@ -1,16 +1,14 @@
 import { Typography, Button, Card, CardContent } from "@mui/material";
+import { AppLayout } from "../../layouts/AppLayout";
 
 export const JournalPage = () => {
   return (
-    <>
-      <Button variant="outlined" size="small">
-        Outlined
-      </Button>
-      <Card>
-        <CardContent>
-          <Typography>JournalPage</Typography>
-        </CardContent>
-      </Card>
-    </>
+    <AppLayout>
+      <Typography>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed corrupti,
+        eius, quasi odit, itaque consectetur modi culpa quisquam temporibus sunt
+        laboriosam est odio ab magnam! Error repellat natus sed eum!
+      </Typography>
+    </AppLayout>
   );
 };
